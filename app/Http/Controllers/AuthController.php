7787921ed2db8cd0e2b1d96ea7ae0392900b05ca
mhill <markhousemd@yahoo.com>
2021-03-03 11:05:13 -0600

    adding in the login method to the AuthController

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class AuthController extends Controller
+{
+
+    public function login(Request $request)
+    {
+        $credentials = $request->only('username', 'password');
+        
+        if(Auth::attempt(['username' => $credentials['username'], 'password' => md5($credentials['password'])], $remember = true)) {
+            $request->session()->regenerate();
+            return true;
+        }else {
+            return md5($credentials['password']);
+        }
+        return false;
+
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,10 @@
 <?php
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\CardController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,4 +19,10 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::get('/card', [CardController::class, 'index']);
+
+Route::prefix('auth')->group(function() {
+    Route::post('login', [AuthController::class, 'login']);
 });
