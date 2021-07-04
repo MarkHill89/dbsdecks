0cdc137d9c5ddcb4156978d63639a9bbf0b575e1
Mark <dbsdecks@gmail.com>
2021-07-04 14:53:33 -0500

    adding in the touch ups for the card db page

--- resources/frontend/angular/src/app/cards/cards.component.ts
+++ resources/frontend/angular/src/app/cards/cards.component.ts
@@ -1,9 +1,14 @@
-import { Component, OnInit, ViewEncapsulation, OnDestroy } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation, OnDestroy, HostListener, Inject } from '@angular/core';
 import { CardsQuery } from './state/cards.query';
 import { CardsService } from './state/cards.service';
 import { tap } from 'rxjs/operators';
 import { Observable, Subscription } from 'rxjs';
 import { Card } from './state/card.model';
+import { CardFilterModalComponent } from '../shared/modals/card-filter-modal/card-filter-modal.component';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { FilterCards } from '@dbsdecks/app/infrastructure/classes/card-filter.class';
+import { DOCUMENT } from '@angular/common';
+import { CardInfoModalComponent } from '../shared/modals/card-info-modal/card-info-modal.component';
 
 @Component({
   selector: 'cards-component',
@@ -14,13 +19,18 @@ export class CardsComponent implements OnInit, OnDestroy{
   
   private cards: Card[] = [];
   private scrollSum = 24;
+  private cardFilters = {};
 
   subscriptions: Subscription  = new Subscription;
   scrollCards: Card[] = [];
 
+  windowScrolled: boolean = false;
+
   constructor(
+    @Inject(DOCUMENT) private document: Document,
     private query: CardsQuery,
-    private cardsService: CardsService
+    private cardsService: CardsService,
+    private modal: NgbModal,
   ) { }
 
   ngOnInit(): void {
@@ -34,8 +44,27 @@ export class CardsComponent implements OnInit, OnDestroy{
     }));
   }
 
+  @HostListener("window:scroll", [])
+  onWindowScroll() {
+      if(window.pageYOffset || this.document.documentElement.scrollTop || this.document.body.scrollTop > 100) {
+        this.windowScrolled = true;
+      }else if (this.windowScrolled && window.pageYOffset || this.document.documentElement.scrollTop || this.document.body.scrollTop < 10) {
+        this.windowScrolled = false;
+      }
+  }
+
+  scrollToTop() {
+    (function smoothscroll() {
+        var currentScroll = document.documentElement.scrollTop || document.body.scrollTop;
+        if (currentScroll > 0) {
+            window.requestAnimationFrame(smoothscroll);
+            window.scrollTo(0, currentScroll - (currentScroll / 8));
+        }
+    })();
+  }
+
   onScrollDown() {
-    const cardsToAdd = this.cards.slice(this.scrollSum, this.scrollSum + 12);
+    const cardsToAdd = FilterCards(this.cards, this.cardFilters).slice(this.scrollSum, this.scrollSum + 12);
     this.scrollSum += 12;
     cardsToAdd.forEach(card => {
       this.scrollCards.push(card);
@@ -52,5 +81,22 @@ export class CardsComponent implements OnInit, OnDestroy{
 
   ngOnDestroy() {
     this.subscriptions.unsubscribe();
+  }
+
+  openFilters(title: string) {
+    const modalRef = this.modal.open(CardFilterModalComponent)
+    modalRef.componentInstance.modalTitle = title;
+    modalRef.componentInstance.cardFilters = this.cardFilters;
+    modalRef.componentInstance.cardFilterValues.subscribe((cardFilterValues: any) => {
+      this.scrollSum = 24;
+      this.scrollToTop();
+      this.scrollCards = FilterCards(this.cards, cardFilterValues).slice(0, this.scrollSum);
+      this.cardFilters = cardFilterValues
+    })
+  }
+
+  openCardView(card: Card) {
+    const modalRef = this.modal.open(CardInfoModalComponent)
+    modalRef.componentInstance.card = card;
   }
 }
