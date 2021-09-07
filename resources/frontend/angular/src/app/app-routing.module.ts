5a6ba97806b8f55eb73497fad37e42ecfa45a456
Jose Carranco <jcarranco@tvi-mp3.com>
2021-09-07 15:51:38 -0500

    api build

--- resources/frontend/angular/src/app/app-routing.module.ts
+++ resources/frontend/angular/src/app/app-routing.module.ts
@@ -10,15 +10,15 @@ import { PortalComponent } from './portal/portal.component';
 
 
 const routes: Routes = [
-  { path: 'cards', loadChildren: () => import('./cards/cards.module').then(cards => cards.CardsModule)},
-  { path: 'deck/list', component: DeckListComponent},
-  { path: 'deck/view/:id', component: DeckListViewComponent},
-  { path: 'deckbuilder', loadChildren: () => import('./deck-builder/deck-builder.module').then(deckbuilder => deckbuilder.DeckBuilderModule), canActivate:[AuthService]},
-  { path: 'register/new', component:RegisterComponent},
-  { path: 'login', component:LoginComponent},
-  { path: 'portal', component:PortalComponent, canActivate:[AuthService]},
-  { path: 'home', component: HomeComponent},
-  { path: '**', redirectTo: '/home' }
+  { path: 'dbsdecks/public/cards', loadChildren: () => import('./cards/cards.module').then(cards => cards.CardsModule)},
+  { path: 'dbsdecks/public/deck/list', component: DeckListComponent},
+  { path: 'dbsdecks/public/deck/view/:id', component: DeckListViewComponent},
+  { path: 'dbsdecks/public/deckbuilder', loadChildren: () => import('./deck-builder/deck-builder.module').then(deckbuilder => deckbuilder.DeckBuilderModule), canActivate:[AuthService]},
+  { path: 'dbsdecks/public/register/new', component:RegisterComponent},
+  { path: 'dbsdecks/public/login', component:LoginComponent},
+  { path: 'dbsdecks/public/portal', component:PortalComponent, canActivate:[AuthService]},
+  { path: 'dbsdecks/public/home', component: HomeComponent},
+  { path: '**', redirectTo: '/dbsdecks/public/home' }
 ];
 
 @NgModule({
