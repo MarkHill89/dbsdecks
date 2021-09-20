cd76627f8f4caaf074bf2954dd4f1ed06e07d685
Jose Carranco <jcarranco@tvi-mp3.com>
2021-09-20 16:55:46 -0500

    deck builder import data on landing

--- app/Http/Controllers/CardController.php
+++ app/Http/Controllers/CardController.php
@@ -30,12 +30,13 @@ class CardController extends Controller
         return $dataService->getAllDecks($isPublic, $leaderCardNumber, $limit);
     }
 
-    public function get_deck_by_user(Request $request){
+    public function get_deck_by_user(Request $request)
+    {
         $decksByUser = DB::table('deck')
-                        ->select()
-                        ->where('userId', auth()->user()->id)
-                        ->where('isActive', 1)
-                        ->get();
+            ->select()
+            ->where('userId', auth()->user()->id)
+            ->where('isActive', 1)
+            ->get();
 
 
         return json_decode($decksByUser);
@@ -44,7 +45,6 @@ class CardController extends Controller
     public function get_deck_view_data(Request $request, DataService $dataService)
     {
         return $dataService->getDeckData($request->input('deckId'));
-        
     }
 
 
@@ -72,7 +72,7 @@ class CardController extends Controller
 
         $userId = auth()->user()->id;
         $leader = $input['deck']['leader'];
-        $title = empty($input['deck']['title']) ? auth()->user()->username." ".$leader['cardName'] : $input['deck']['title'];
+        $title = empty($input['deck']['title']) ? auth()->user()->username . " " . $leader['cardName'] : $input['deck']['title'];
         $mainDeck = $input['deck']['mainDeck'];
         $sideDeck = $input['deck']['sideDeck'];
         $isPrivate = $input['deck']['isPrivate'];
@@ -102,8 +102,6 @@ class CardController extends Controller
             ->select('id')
             ->orderBy('id', 'desc')
             ->first();
-
-        DB::table('deck_data_new')->where('deckId', $id->id)->delete();
 
         foreach ($mainDeck as $value) {
             $cardNumber = $value['cardNumber'];
@@ -176,7 +174,7 @@ class CardController extends Controller
 
         $userId = auth()->user()->id;
         $leader = $input['deck']['leader'];
-        $title = empty($input['deck']['title']) ? auth()->user()->username." ".$leader['cardName'] : $input['deck']['title'];
+        $title = empty($input['deck']['title']) ? auth()->user()->username . " " . $leader['cardName'] : $input['deck']['title'];
         $mainDeck = $input['deck']['mainDeck'];
         $sideDeck = $input['deck']['sideDeck'];
         $isPrivate = $input['deck']['isPrivate'];
@@ -192,63 +190,60 @@ class CardController extends Controller
         $sideDeckIndex = 0;
         $id = $input['deck']['id'];
 
-    
+
         DB::table('deck_data_new')->where('deckId', $id)->delete();
 
-        foreach ($mainDeck as $value) {
-            $cardNumber = $value['cardNumber'];
+        // get cardNumbers only
+        $cardNumbersMain = array_column($mainDeck, "cardNumber");
+        $cardNumbersSide = array_column($sideDeck, "cardNumber");
+        $cardNumberCountMain = array_count_values($cardNumbersMain);
+        $cardNumberCountSide = array_count_values($cardNumbersSide);
+        $cardNumbersMainUnique = array_unique($cardNumbersMain);
+        $cardNumbersSideUnique = array_unique($cardNumbersSide);
 
-            // Check to see if the current card number is the same as the previous cardnumber in the loop
-            if ($cardNumber === $currentCardNumber) {
-                // if the card number is the same increment the main quantity +1 and assign the card number as the current card
-                $mainQty++;
-                $currentCard = $cardNumber;
-                // update the arrays 'quantity' value from it's previous to the new +1 value
-                $newMainDeck[$deckIndex - 1]['quantity'] = $mainQty;
-            } else {
-                // if the card number is different start the main quantity over at 1
-                $mainQty = 1;
-                // increment the position of the deck index from the data coming from the website
-                $deckIndex++;
-                $currentCardNumber = $cardNumber;
-                // add a new array inside the $newMainDeck Array with the properties [cardNumber => $currentCardNumber,  quantity => $mainQty]
-                $newMainDeck[] = ['cardNumber' => $currentCardNumber, 'quantity' => $mainQty];
-            }
-        }
+        // build the Unique array from mainDeck
+        $uniqueMainCardArray = array_filter($mainDeck, function ($key, $value) use ($cardNumbersMainUnique) {
+            return in_array($value, array_keys($cardNumbersMainUnique));
+        }, ARRAY_FILTER_USE_BOTH);
 
-        foreach ($newMainDeck as $value) {
-            $cardNumber = $value['cardNumber'];
-            $quantity = $value['quantity'];
+        // build the Unique array from SideDeck
+        $uniqueSideCardArray = array_filter($sideDeck, function ($key, $value) use ($cardNumbersSideUnique) {
+            return in_array($value, array_keys($cardNumbersSideUnique));
+        }, ARRAY_FILTER_USE_BOTH);
 
-            DB::table('deck_data_new')->update(
+        // merge the lists
+        $deckList = array_unique(array_merge($uniqueMainCardArray, $uniqueSideCardArray), SORT_REGULAR);
+
+        $finalList = array_map(function ($d) use ($id, $cardNumberCountMain, $cardNumberCountSide) {
+            return (object) [
+                "deckId" => $id,
+                "cardNumber" => $d['cardNumber'],
+                "mainDeckQty" => isset($cardNumberCountMain[$d['cardNumber']]) ? $cardNumberCountMain[$d['cardNumber']] : 0,
+                "sideDeckQty" => isset($cardNumberCountSide[$d['cardNumber']]) ? $cardNumberCountSide[$d['cardNumber']] : 0
+            ];
+        }, $deckList);
+
+        foreach ($finalList as $list) {
+            $cardNumber = $list->cardNumber;
+            $mQuantity = $list->mainDeckQty;
+            $sQuantity = $list->sideDeckQty;
+
+
+            DB::table('deck_data_new')->updateOrInsert(
                 ['deckId' => $id, 'cardNumber' => $cardNumber],
-                ['mainDeckQty' => $quantity]
+                ['mainDeckQty' => $mQuantity, 'sideDeckQty' => $sQuantity]
             );
+
+            DB::table('deck')
+                ->where('id', $id)
+                ->update([
+                    'title' => $title,
+                    'leaderNumber' => $leaderCardNumber,
+                    'isPrivate' => $isPrivate
+                ]);
         }
 
-        foreach ($sideDeck as $sideValue) {
-            $cardNumber = $sideValue['cardNumber'];
-            if ($cardNumber === $currentCardNumber) {
-                $mainQty++;
-                $currentCard = $cardNumber;
-                $newSideDeck[$sideDeckIndex - 1]['quantity'] = $mainQty;
-            } else {
-                $mainQty = 1;
-                $sideDeckIndex++;
-                $currentCardNumber = $cardNumber;
-                $newSideDeck[] = ['cardNumber' => $currentCardNumber, 'quantity' => $mainQty];
-            }
-        }
 
-        foreach ($newSideDeck as $newSideValue) {
-            $cardNumber = $newSideValue['cardNumber'];
-            $quantity = $newSideValue['quantity'];
-
-            DB::table('deck_data_new')->update(
-                ['deckId' => $id, 'cardNumber' => $cardNumber],
-                ['sideDeckQty' => $quantity]
-            );
-        }
         return response([
             'id' => $id
         ], 200);
@@ -264,20 +259,17 @@ class CardController extends Controller
         if (Auth::check()) {
             DB::table('deck')
                 ->where('id', $id)
-                ->update(['isActive' => 0]
-            );
-    
-            DB::table('deck_data_new')->where('deckId', $id)->delete();
+                ->update(
+                    ['isActive' => 0]
+                );
+
             return response([
                 'message' => 'Deck Deleted'
-             ], 200);
-         } else {
-             return response([
+            ], 200);
+        } else {
+            return response([
                 'message' => 'Not Authorized'
-             ], 400);
-         }
-
-
-
+            ], 400);
+        }
     }
 }
