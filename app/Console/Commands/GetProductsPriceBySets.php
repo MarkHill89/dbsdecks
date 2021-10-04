05e86907606c4ff19b03fa7d03eebfbf1f2e04b5
Jose Carranco <jcarranco@tvi-mp3.com>
2021-10-04 15:27:07 -0500

    add tcgplayer pricing script

--- app/Console/Commands/GetProductsPriceBySets.php
+++ app/Console/Commands/GetProductsPriceBySets.php
@@ -4,6 +4,7 @@ namespace App\Console\Commands;
 
 use Illuminate\Console\Command;
 use App\Helpers\Proc;
+use Illuminate\Support\Facades\DB;
 
 class GetProductsPriceBySets extends Command
 {
@@ -19,7 +20,7 @@ class GetProductsPriceBySets extends Command
      *
      * @var string
      */
-    protected $description = 'This will return all the price for all the cards for each individual set. TCGPlayers limit is set to 10 by defauls and only allow up to 100 results per query. So the limit parameter is added to the url to maximize the output.';
+    protected $description = 'This will return all the price for all the cards for each individual set. TCGPlayers limit is set to 100 by default and only allow up to 100 results per query. So the limit parameter is added to the url to maximize the output.';
 
     /**
      * Create a new command instance.
@@ -45,36 +46,62 @@ class GetProductsPriceBySets extends Command
         $authData = "grant_type=client_credentials&client_id=$publicKey&client_secret=$privateKey";
         $bearerToken = Proc::getTcgBearerToken();
 
-        $groupId = 2796; // GroupId for Supreme Rivalry
+        $groupIds = DB::table('tcgplayer_group')
+            ->select('groupId')
+            ->get();
+
         $offset = 0;
 
-        $apiUrl = "https://api.tcgplayer.com/pricing/group/$groupId?limit=100&offset=$offset";
+        foreach ($groupIds as $setId) {
+            $groupId = $setId->groupId;
 
-        curl_setopt_array($curl, [
-            CURLOPT_URL => $apiUrl,
-            CURLOPT_POSTFIELDS => $authData,
-            CURLOPT_RETURNTRANSFER => true,
-            CURLOPT_ENCODING => "",
-            CURLOPT_MAXREDIRS => 10,
-            CURLOPT_TIMEOUT => 30,
-            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-            CURLOPT_CUSTOMREQUEST => "GET",
-            CURLOPT_HTTPHEADER => [
-                "Accept: application/json",
-                "Authorization: bearer $bearerToken"
-            ],
-        ]);
+            $apiUrl = "https://api.tcgplayer.com/pricing/group/$groupId?limit=1&offset=$offset";
 
-        $response = curl_exec($curl);
-        $err = curl_error($curl);
+            curl_setopt_array($curl, [
+                CURLOPT_URL => $apiUrl,
+                CURLOPT_POSTFIELDS => $authData,
+                CURLOPT_RETURNTRANSFER => true,
+                CURLOPT_ENCODING => "",
+                CURLOPT_MAXREDIRS => 10,
+                CURLOPT_TIMEOUT => 30,
+                CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+                CURLOPT_CUSTOMREQUEST => "GET",
+                CURLOPT_HTTPHEADER => [
+                    "Accept: application/json",
+                    "Authorization: bearer $bearerToken"
+                ],
+            ]);
+
+            $response = curl_exec($curl);
+            $err = curl_error($curl);
+
+            if ($err) {
+                echo "cURL Error #:" . $err;
+            } else {
+                $jsonData = json_decode($response);
+                $data = $jsonData->results;
+
+                foreach ($data as $product) {
+                    DB::table('tcgplayer_price_dupetest')
+                        ->updateOrInsert(
+                            ['productId' => $product->productId],
+                            [
+                                'lowPrice' => $product->lowPrice,
+                                'midPrice' => $product->midPrice,
+                                'highPrice' => $product->highPrice,
+                                'marketPrice' => $product->marketPrice,
+                                'directLowPrice' => $product->directLowPrice,
+                                'subTypeName' => $product->subTypeName,
+                                'updateDate' => now()
+
+                            ]
+                        );
+                    echo "added price for $product->productId ";
+                }
+            }
+        }
 
         curl_close($curl);
-
-        if ($err) {
-            echo "cURL Error #:" . $err;
-        } else {
-            echo $response;
-        }
 
         return "Get Products By Sets successfully ran";
     }
